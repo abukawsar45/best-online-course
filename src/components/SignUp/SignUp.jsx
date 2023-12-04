84e0336d3d19f0ca7fe24b424428b0ce2bfa28ea
abukawsar45 <kawsar1146236@gmail.com>
2023-12-04 12:46:25 +0600

    handle register page

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,115 +1,150 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import loginImage from './../../assets/login.svg';
+import toast, { Toaster } from 'react-hot-toast';
 import { FcGoogle } from 'react-icons/fc';
+import { useForm, useWatch } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser } from '../../redux/features/user/userSlice';
 
 const SignUp = () => {
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
+  const { handleSubmit, register, control } = useForm();
+  const password = useWatch({ control, name: 'password' });
+  const confirmPassword = useWatch({ control, name: 'confirmPassword' });
+  const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(true);
+
+  const ab = useSelector((state) => state.userSlice);
+  const {email, isLoading, isError, error } = ab || {};
+  console.log(ab)
+  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (
+      password !== undefined &&
+      password !== '' &&
+      confirmPassword !== undefined &&
+      confirmPassword !== '' &&
+      password === confirmPassword
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [password, confirmPassword]);
+
+  useEffect(() => {
+    if (isError && error)
+    {
+      toast.error(error);
+      console.log(' last')
+    }
+  }, [isError, error]);
+
+  useEffect(() => {
+    if (!isLoading && email)
+    {
+      navigate('/')
+      toast.success('Signup successfull')
+    }
+  },[isLoading, email])
+
+
+  const onSubmit = ({ name, email, password }) => {
+    dispatch(createUser({
+      email,
+      password,
+      name,
+    }))
+    console.log(name, email, password);
+  };
+
+  const handleGoogleLogin = () => {
+    // Google Login
+  };
+
   return (
-    <div className='flex justify-center items-center py-4'>
-      <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
-        <div className='mb-8 text-center'>
-          <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
-          <p className='text-sm text-gray-400'>Welcome to AirCNC</p>
-        </div>
-        <form
-          noValidate=''
-          action=''
-          className='space-y-6 ng-untouched ng-pristine ng-valid'
-        >
-          <div className='space-y-4'>
-            <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
-                Name
-              </label>
+    <div className='flex max-w-7xl mx-auto h-screen items-center'>
+      <div className='w-1/2'>
+        <img src={loginImage} className='h-full w-full' alt='' />
+        <Toaster />
+      </div>
+      <div className='w-1/2  grid place-items-center'>
+        <div className='bg-slate-200 w-full max-w-sm rounded-lg grid place-items-center p-10'>
+          <h1 className='mb-10 font-medium text-2xl'>Sign up</h1>
+          <form className='space-y-5 w-full' onSubmit={handleSubmit(onSubmit)}>
+            <div className='flex flex-col items-start'>
+              <label htmlFor='email'>Name</label>
               <input
                 type='text'
-                name='name'
                 id='name'
-                placeholder='Enter Your Name Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
+                className='w-full py-2 px-3 rounded-md'
+                {...register('name')}
               />
             </div>
-            <div>
-              <label htmlFor='image' className='block mb-2 text-sm'>
-                Select Image:
-              </label>
-              <input
-                required
-                type='file'
-                id='image'
-                name='image'
-                accept='image/*'
-              />
-            </div>
-            <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
-                Email address
-              </label>
+            <div className='flex flex-col items-start'>
+              <label htmlFor='email'>Email</label>
               <input
                 type='email'
-                name='email'
                 id='email'
-                required
-                placeholder='Enter Your Email Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
+                className='w-full py-2 px-3 rounded-md'
+                {...register('email')}
               />
             </div>
-            <div>
-              <div className='flex justify-between'>
-                <label htmlFor='password' className='text-sm mb-2'>
-                  Password
-                </label>
-              </div>
+            <div className='flex flex-col items-start'>
+              <label htmlFor='password'>Password</label>
               <input
                 type='password'
-                name='password'
                 id='password'
-                required
-                placeholder='*******'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
+                className='w-full py-2 px-3 rounded-md'
+                {...register('password')}
               />
             </div>
-          </div>
-
-          <div>
+            <div className='flex flex-col items-start'>
+              <label htmlFor='confirm-password'>Confirm Password</label>
+              <input
+                type='password'
+                id='confirm-password'
+                className='w-full py-2 px-3 rounded-md'
+                {...register('confirmPassword')}
+              />
+            </div>
+            <div className='!mt-8 '>
+              <button
+                type='submit'
+                className='px-3 py-2 rounded-full hover:text-white font-josefin bg-blue-400 hover:bg-blue-600 w-full disabled:bg-gray-300 disabled:cursor-not-allowed'
+                disabled={disabled}
+              >
+                Sign up
+              </button>
+            </div>
+            <div>
+              <p>
+                Already have an account?{' '}
+                <span
+                  className='text-blue-500 hover:underline cursor-pointer'
+                  onClick={() => navigate('/login')}
+                >
+                  Login
+                </span>
+              </p>
+            </div>
             <button
-              type='submit'
-              className='bg-rose-500 w-full rounded-md py-3 text-white'
+              type='button'
+              onClick={handleGoogleLogin}
+              className='flex justify-center items-center space-x-2 border m-3 font-josefin border-gray-300 hover:bg-blue-500 hover:text-white bg-white  border-rounded cursor-pointer px-3 py-2 rounded-lg'
             >
-              Continue
-            </button>
-          </div>
-        </form>
-        <div className='flex items-center pt-4 space-x-1'>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-          <p className='px-3 text-sm dark:text-gray-400'>
-            Signup with social accounts
-          </p>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-        </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
-          <FcGoogle size={32} />
+              <FcGoogle size={32} />
 
-          <p>Continue with Google</p>
+              <p>Continue with Google</p>
+            </button>
+          </form>
         </div>
-        <p className='px-6 text-sm text-center text-gray-400'>
-          Already have an account?{' '}
-          <Link
-            to='/login'
-            className='hover:underline hover:text-rose-500 text-gray-600'
-          >
-            Login
-          </Link>
-          .
-        </p>
       </div>
     </div>
   );
 };
+
 
 export default SignUp;
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.MY_apiKey,
-  authDomain: import.meta.env.MY_authDomain,
-  projectId: import.meta.env.MY_projectId,
-  storageBucket: import.meta.env.MY_storageBucket,
-  messagingSenderId: import.meta.env.MY_messagingSenderId,
-  appId: import.meta.env.MY_appId,
+  apiKey: "AIzaSyDPfQrclCvoyhLPOU8qEm_BnQwGwfZRjXc",
+  authDomain: "tech-camp-1864d.firebaseapp.com",
+  projectId: "tech-camp-1864d",
+  storageBucket: "tech-camp-1864d.appspot.com",
+  messagingSenderId: "589397933769",
+  appId: "1:589397933769:web:9bbb31572f86cd17e69718"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-
+const auth= getAuth(app);
 export default auth;
